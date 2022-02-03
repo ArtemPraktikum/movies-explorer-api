90344dcb136901136fa315382b3a8dc010523e92
Artem Velmozhin <artemvrabota@yandex.ru>
2022-02-04 00:20:42 +0400

    feat: bd + mongoose

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-//функциональность точки входа
-const express = require('express');
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
-const app = express();
-
 // импорты
 const router = require('./routes/index.js');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+//функциональность точки входа
+const app = express();
 
 
-
+// подключение к бд
+mongoose.connect('mongodb://localhost:27017/moviesdb').then(() => {
+  // Если бд работает, консоль подтвердит
+  console.log('Connected to the Database. Yayzow!');
+})
 
 // подключить bobyParser как middleware
 app.use(bodyParser.json());
