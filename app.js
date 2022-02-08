f1fbb02e9c428142c3df9da2f71a6cf38f714294
Artem Velmozhin <artemvrabota@yandex.ru>
2022-02-09 02:49:35 +0400

    feat: helmet, rate-limiter, dotenv, lint все: настроить/подключить

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // импорты
+require('dotenv').config();
 const router = require('./routes/index.js');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const express = require('express');
+const helmet = require("helmet");
+const rateLimit = require('express-rate-limit');
+
+
+// настройка лимитера
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // за 15 минут
+  max: 100 // можно совершить максимум 100 запросов с одного IP
+});
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -16,8 +26,10 @@ mongoose.connect('mongodb://localhost:27017/moviesdb').then(() => {
   console.log('Connected to the Database. Yayzow!');
 })
 
-// подключить bobyParser как middleware
+// подключить middlewares
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(limiter);
 // единый роутер для всего
 app.use('/', router);
 
